e778f2a650b94b4ad2f98e9a6b2677427daa610d
rufeng.wei <rufeng.wei@seamoney.com>
2023-10-23 09:35:04 +0800

    feat(unit-test): add unit-test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// 计算两个整数的最大公约数
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    // 这里的 n 作为结果返回，不需要以分号结尾
+    n
+}
+
+// Rust 自带单元测试
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11)
+}
